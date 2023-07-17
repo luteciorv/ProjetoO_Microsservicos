365a2bf086f40a930755befc20f2833537c9de65
Luís Felipe <luisrodrigues@bne.com.br>
2023-07-17 11:19:40 -0300

    Implementação de Retries com Exponencial backoff em requisições Http Síncronas - Play.Inventory

--- Play.Inventory.Service/Program.cs
+++ Play.Inventory.Service/Program.cs
@@ -2,6 +2,7 @@ using Play.Commom.Service.Extensions;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
 using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +10,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.ConfigureMongoDb();
 builder.Services.ConfigureMongoRepository<InventoryItem>("inventoryitems");
 
+var jitterer = new Random();
+
 builder.Services.AddHttpClient<CatalogClient>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:7209/api/");
 })
+.AddTransientHttpErrorPolicy(b => b.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+    5, 
+    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000))
+))
 .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 builder.Services.AddControllers();
