4a06fa02486f3b66f6b9764350443c5caa8d989a
Luís Felipe <luisrodrigues@bne.com.br>
2023-07-17 11:02:31 -0300

    Adição do package Polly ao Play.Inventory para lidar com erros parciais de chamadas Http Síncronas

--- Play.Inventory.Service/Program.cs
+++ Play.Inventory.Service/Program.cs
@@ -1,6 +1,7 @@
 using Play.Commom.Service.Extensions;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,8 @@ builder.Services.ConfigureMongoRepository<InventoryItem>("inventoryitems");
 builder.Services.AddHttpClient<CatalogClient>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:7209/api/");
-});
+})
+.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
